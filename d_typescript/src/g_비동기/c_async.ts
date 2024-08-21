b46088f437d0cf0e05340cac426e852d247e016f
Heydia001 <makamong0000@gmail.com>
2024-08-21 15:33:02 +0900

    2240821

---
+++ d_typescript/src/g_비동기/c_async.ts
@@ -0,0 +1,15 @@
+//c_async.ts
+export const tmp = '';
+
+//! Async & Await
+// : 프로미스를 기반으로 비동기 작업을 간편하게 작성하는 방법
+
+// : async로 정의 된 함수 내에서
+// : await 키워드를 사용하여 비동기 작업의 결과를 기다림
+
+//? 1. async 함수 내에서만 await 사용아ㅣ 가능
+//? 2. 동기 코드와 유사한 형태로 비동기 코드 작성
+
+function fetchUserData() {
+  
+}
